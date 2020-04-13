c60d7920899b9d5e8b01a7187d0a698d0f9e026c
MariaPlastun <63410203+MariaPlastun@users.noreply.github.com>
2020-04-13 12:43:26 +0500

    Создать add.h

---
+++ add.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef ADD_H
+#define ADD_H
+
+int add(int a, int b, int c);
+int add(int a, int b, int c, int d);
+float add(float a, float b, float c);
+double add(double a, double b);
+#endif
